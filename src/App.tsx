14c27b5986b90244dba11714eaa4be77d472f7cc
Ms.PATIPHON KLANGPRAPHAN <63130500216@st.sit.kmutt.ac.th>
2023-07-08 20:15:08 +0700

    first init ✨

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 style={{ color: rgba(217, 69, 151, 1)}}>Hello, Patiphon</h1>
+    </>
+  )
+}
+
+export default App
